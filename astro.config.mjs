a2e7d53f3ff57e26a63f2eb63af8d670d51fff56
Ignacio A. Molina <strocsdev@gmail.com>
2024-06-03 21:35:57 -0400

    add: soon_page to main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+
+import sitemap from '@astrojs/sitemap'
+import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/static'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), react(), tailwind()],
+  output: 'static',
+  adapter: vercel()
+})
---
+++ src/data/RESULTS.ts
@@ -0,0 +1,28 @@
+
+interface RESULT {
+  title: string
+  docURL: string
+}
+
+export const RESULTS: RESULT[] = [
+  {
+    title: 'Artes Escénicas',
+    docURL: '#'
+  },
+  {
+    title: 'Artes Visuales',
+    docURL: '#'
+  },
+  {
+    title: 'Audiovisual',
+    docURL: '#'
+  },
+  {
+    title: 'Literatura',
+    docURL: '#'
+  },
+  {
+    title: 'Música',
+    docURL: '#'
+  },
+]
