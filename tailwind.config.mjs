5c7d9c723603408cf782b5061ecb9d886d4f4cff
Ignacio A. Molina <strocsdev@gmail.com>
2024-06-18 14:51:56 -0400

    add: soon page 2.0

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,11 +5,34 @@ export default {
     extend: {
       colors: {
         primary: '#ee0175',
-        secondary: '#f1f1f1'
+        secondary: '#f1f1f1',
+        'accent-yellow': '#fcea0d',
+        'accent-blue': '#1d70b7',
+        'day-1': '#04bfb1',
+        'day-2': '#662482',
+        'day-3': '#d9e021',
+        'day-4': '#f29100'
       }
     },
+    backgroundImage: {
+      'hero-image': "url('hero-image.webp')",
+      'snow-ball': "url('snowflake-1.png')",
+      'snow-star': "url('snowflake-2.png')"
+    },
     fontFamily: {
-      secondary: ['Fira Sans', 'sans-serif']
+      fira: ['Fira Sans', 'sans-serif', 'system-ui'],
+      climate: ['Climate Crisis', 'sans-serif', 'system-ui']
+    },
+    keyframes: {
+      snowflakes: {
+        '0%': { transform: 'scale(1) rotate(0deg)' },
+        '100%': {
+          transform: 'scale(1.2) rotate(20deg)'
+        }
+      }
+    },
+    animation: {
+      snowflakes: 'snowflakes 200ms steps(2,end) alternate infinite'
     }
   },
   plugins: []
