75429af5211a0b63c448a171a5bc97972ce83828
Ignacio A. Molina <strocsdev@gmail.com>
2024-06-19 17:11:22 -0400

    add: enums for constant texts

--- src/data/CATEGORIES.ts
+++
@@ -1,8 +0,0 @@
-const CATEGORIES = {
-  visuales: 'Artes Visuales',
-  musica: 'Música',
-  literatura: 'Literatura',
-  audiovisual: 'Audiovisual',
-  escenicas: 'Artes Escénicas',
-  artesania: 'Artesanía'
-}
---
+++ src/data/CONSTANTS.ts
@@ -0,0 +1,33 @@
+export const CATEGORIES = {
+  visuales: 'Artes Visuales',
+  musica: 'Música',
+  literatura: 'Literatura',
+  audiovisual: 'Audiovisual',
+  escenicas: 'Artes Escénicas',
+  artesania: 'Artesanía'
+}
+
+
+export const CITIES = {
+  ls: 'La Serena',
+  cq: 'Coquimbo',
+  ov: 'Ovalle',
+  ill: 'Illapel'
+}
+
+
+export const PLACES = {
+  consistorial: { name: 'Auditorio Consistorial Municipalidad de Coquimbo', maps: 'https://maps.app.goo.gl/gjx2duh2gBYcgavS9' },
+  salaExposiciones: { name: 'Sala de Exposiciones ULS', maps: 'https://maps.app.goo.gl/pisAF9ngFG5T4axT8' },
+  kamino: { name: 'Centro Cultural KAMINO520', maps: 'https://maps.app.goo.gl/cvmGA86tmypHZBdi6' },
+  teatroMunicipal: { name: 'Teatro Municipal', maps: 'https://maps.app.goo.gl/y7FDYBx99Rqe74zp9' },
+  salonAuditorioIllapel: { name: 'Salón Auditorio Casa de la Cultura', maps: '' },
+  casaEditorial: { name: 'Casa Editorial ULS', maps: 'https://maps.app.goo.gl/EEebachieKw2XMFg8' },
+  plazaIllapel: { name: 'Plaza de Armas de Illapel', maps: '' },
+  tmo: { name: 'TMO', maps: '' },
+  aulaMagna: { name: 'Aula Magna ULS', maps: '' },
+  palace: { name: 'Centro Cultural Palace', maps: '' },
+  museoArqueologico: { name: 'Salón Auditorio "Francisco Cornely" - Museo Arqueológico', maps: '' },
+  centenario: { name: 'Teatro Centenario', maps: '' }
+
+}
