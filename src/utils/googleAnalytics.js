eaa8806510fb40d745449cec8dc04bbb0d9dce08
Ignacio A. Molina <strocsdev@gmail.com>
2024-06-21 12:40:58 -0400

    fix: GA4

--- src/utils/googleAnalytics.js
+++
@@ -1,9 +0,0 @@
-window.dataLayer = window.dataLayer || []
-const gtag = () => {
-  window.dataLayer.push(arguments)
-}
-gtag('js', new Date())
-
-gtag('config', 'G-84D3X270SY')
-
-console.log('GA4 Running')
