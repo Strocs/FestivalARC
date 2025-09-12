849b0b89a1e8618e46b68ee08b9f9adba803be3f
Strocs <strocsdev@gmail.com>
2025-09-11 21:40:08 -0300

    chore: remove unused vercel adapter from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,10 @@ import { defineConfig } from 'astro/config'
 
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/static'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap(), react(), tailwind()],
   output: 'static',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  })
 })
