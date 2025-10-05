7f9ec2e0b35177767fe2481f153dd17c51414db6
Strocs <strocsdev@gmail.com>
2025-10-05 19:23:09 -0300

    refactor(arc2025/schedule): update data/index to new schedule mapping and layout

--- src/features/arc2025/sections/schedule/data/index.ts
+++ src/features/arc2025/sections/schedule/data/index.ts
@@ -1,5 +1,4 @@
 // Repository like to parse data from data-source to schedule input (Just an example, data can actually pass directly if came well )
-import { createScheduleGrid, type Event, type ScheduleInput, type Track } from '@/features/schedule'
 
 import stages from './stages.json' with { type: 'json' }
 import day1Config from './day1/config.json' with { type: 'json' }
@@ -7,6 +6,8 @@ import day2Config from './day2/config.json' with { type: 'json' }
 
 import { day1Events } from './day1'
 import { day2Events } from './day2'
+import type { Arc2025Event, Arc2025Stage } from '../types'
+import { createScheduleLayout, type ScheduleInput } from '@/features/schedule'
 
 interface Arc2025Data {
   time: {
@@ -14,43 +15,68 @@ interface Arc2025Data {
     end: string
     interval: number
   }
-  stages: Track[]
-  events: Event[]
+  stages: Arc2025Stage[]
+  events: Arc2025Event[]
 }
 
-export function Arc2025ScheduleRepository(data: Arc2025Data): ScheduleInput {
+export function mapArc2025DataToScheduleInput(
+  data: Arc2025Data,
+): ScheduleInput {
   // get data from data source and parse to ScheduleInput
   return {
-    time: {
+    scheduleTime: {
       start: data.time.start,
       end: data.time.end,
-      intervalMinutes: data.time.interval
+      intervalMinutes: data.time.interval,
     },
-    tracks: data.stages,
-    events: data.events
-  }
+    tracks: data.stages.map((stage) => ({
+      id: stage.id,
+      order: stage.order,
+      payload: {
+        name: stage.name,
+        color: stage.color,
+      },
+    })),
+    events: data.events.map((event) => ({
+      id: event.id,
+      trackId: event.trackId,
+      time: {
+        start: event.time.start,
+        end: event.time.end,
+      },
+      payload: {
+        title: event.title,
+        subTitle: event.subTitle,
+        description: event.description,
+        category: event.metadata.category,
+        duration: event.metadata.duration,
+        galleryUrl: event.metadata.galleryUrl,
+      },
+    })),
+  } satisfies ScheduleInput
 }
 
-const scheduleDay1 = Arc2025ScheduleRepository({
+const arc2025Day1InputData = mapArc2025DataToScheduleInput({
   time: {
     start: day1Config.startTime,
     end: day1Config.endTime,
-    interval: day1Config.scheduleInterval
+    interval: day1Config.scheduleInterval,
   },
   stages: stages,
-  events: day1Events
+  events: day1Events,
 })
 
-const scheduleDay2 = Arc2025ScheduleRepository({
+const arc2025Day2InputData = mapArc2025DataToScheduleInput({
   time: {
     start: day2Config.startTime,
     end: day2Config.endTime,
-    interval: day2Config.scheduleInterval
+    interval: day2Config.scheduleInterval,
   },
   stages: stages,
-  events: day2Events
+  events: day2Events,
 })
 
-
-export const scheduleDay1GridData = createScheduleGrid(scheduleDay1);
-export const scheduleDay2GridData = createScheduleGrid(scheduleDay2);
+export const arc2025Day1ScheduleLayout =
+  createScheduleLayout(arc2025Day1InputData)
+export const arc2025Day2ScheduleLayout =
+  createScheduleLayout(arc2025Day2InputData)
