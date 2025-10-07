7f1f4526ff5bb0eff8340e074e0ab57b269d597e
Strocs <strocsdev@gmail.com>
2025-10-07 00:45:09 -0300

    refactor(shared/components): adapta ScheduleGridEvents a nueva estructura de columnas

--- src/features/shared/components/ScheduleGridEvents.tsx
+++ src/features/shared/components/ScheduleGridEvents.tsx
@@ -1,8 +1,8 @@
-import { Fragment } from 'react'
 import { ScheduleItem } from './ScheduleItem'
 import { ScheduleEventGroup } from './ScheduleEventGroup'
 import { cn } from '../utils'
 import type { IndividualSlot, GroupSlot } from './types'
+import { ScheduleGridColumn } from './ScheduleGridColumn'
 
 interface Track {
   id: string
@@ -19,6 +19,7 @@ interface FilteredRow {
 interface ScheduleGridEventsProps {
   filteredRows: FilteredRow[]
   tracks: Track[]
+  rowsLength: number
 }
 
 function isGroupSlot(slot: IndividualSlot | GroupSlot): slot is GroupSlot {
@@ -28,62 +29,57 @@ function isGroupSlot(slot: IndividualSlot | GroupSlot): slot is GroupSlot {
 export function ScheduleGridEvents({
   filteredRows,
   tracks,
+  rowsLength,
 }: ScheduleGridEventsProps) {
-  return (
-    <>
-      {filteredRows.map(({ track, slots }, trackIndex) => {
-        return (
-          <Fragment key={track.id}>
-            {slots.map((item) => {
-              if (isGroupSlot(item)) {
-                return (
-                  <ScheduleEventGroup
-                    key={`group-${track.id}-${item.position.start}`}
-                    group={item}
-                    trackIndex={trackIndex}
-                    trackColor={tracks[trackIndex].color}
-                    trackName={tracks[trackIndex].name}
-                  />
-                )
-              }
+  return filteredRows.map(({ track, slots }, trackIndex) => {
+    return (
+      <ScheduleGridColumn key={track.id} rowsLength={rowsLength} header={track}>
+        {slots.map((item) => {
+          if (isGroupSlot(item)) {
+            return (
+              <ScheduleEventGroup
+                key={`group-${track.id}-${item.position.start}`}
+                group={item}
+                trackColor={tracks[trackIndex].color}
+                trackName={tracks[trackIndex].name}
+              />
+            )
+          }
 
-              const adjustedStart = item.position.start * 2 + 3
-              const adjustedSpan = item.position.span * 2 - 1
+          const adjustedStart = item.position.start + 2
+          const adjustedSpan = item.position.span
 
-              return (
-                <div
-                  key={item.slot.trackId + '-' + item.slot.id}
-                  className={cn(
-                    'group relative',
-                    item.position.span > 1 && 'bg-white/30',
-                  )}
-                  style={{
-                    gridRow: `${adjustedStart} / span ${adjustedSpan}`,
-                    gridColumn: trackIndex + 2,
-                  }}>
-                  <ScheduleItem
-                    id={item.slot.id}
-                    trackId={item.slot.trackId}
-                    eventTime={{
-                      start: item.slot.body.time.start,
-                      end: item.slot.body.time.end,
-                    }}
-                    header={item.slot.header}
-                    footer={{
-                      leftButton: item.slot.footer.infoButton,
-                      rightLink: item.slot.footer.inscription,
-                    }}
-                    activityType={item.slot.labels?.left}
-                    duration={item.slot.body.duration}
-                    color={tracks[trackIndex].color}
-                    location={tracks[trackIndex].name}
-                  />
-                </div>
-              )
-            })}
-          </Fragment>
-        )
-      })}
-    </>
-  )
+          return (
+            <div
+              key={item.slot.trackId + '-' + item.slot.id}
+              className={cn(
+                'group relative',
+                item.position.span > 1 && 'bg-25-white/30',
+              )}
+              style={{
+                gridRow: `${adjustedStart} / span ${adjustedSpan}`,
+              }}>
+              <ScheduleItem
+                id={item.slot.id}
+                trackId={item.slot.trackId}
+                eventTime={{
+                  start: item.slot.body.time.start,
+                  end: item.slot.body.time.end,
+                }}
+                header={item.slot.header}
+                footer={{
+                  leftButton: item.slot.footer.infoButton,
+                  rightLink: item.slot.footer.inscription,
+                }}
+                activityType={item.slot.labels?.left}
+                duration={item.slot.body.duration}
+                color={tracks[trackIndex].color}
+                location={tracks[trackIndex].name}
+              />
+            </div>
+          )
+        })}
+      </ScheduleGridColumn>
+    )
+  })
 }
