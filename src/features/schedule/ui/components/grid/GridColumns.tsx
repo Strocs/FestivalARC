70d525a7e1b5dd2e6eb641a4a2bf99281bc047df
Strocs <strocsdev@gmail.com>
2025-10-07 22:43:20 -0300

    refactor(schedule/ui): adapta GridColumns a nueva estructura de listas

--- src/features/schedule/ui/components/grid/GridColumns.tsx
+++ src/features/schedule/ui/components/grid/GridColumns.tsx
@@ -24,62 +24,60 @@ export function GridColumns({
   rowsLength,
   config,
 }: GridColumnsProps) {
-  return filteredRows.map(({ header, column }) => {
-    return (
-      <GridColumn
-        key={header.id}
-        length={{
-          columns: filteredRows.length,
-          rows: rowsLength,
-        }}
-        header={header}
-        config={config}>
-        {column.map((item) => {
-          if (isGroupColumn(item)) {
-            return (
-              <EventGroup
-                key={`group-${header.id}-${item.position.start}`}
-                group={item}
-                header={{
-                  id: header.id,
-                  color: header.color,
-                  name: header.name,
-                }}
-              />
-            )
-          }
-
+  return filteredRows.map(({ header, column }) => (
+    <GridColumn
+      key={header.id}
+      length={{
+        columns: filteredRows.length,
+        rows: rowsLength,
+      }}
+      header={header}
+      config={config}>
+      {column.map((item) => {
+        if (isGroupColumn(item)) {
           return (
-            <div
-              key={item.columnData.id}
-              className={cn(
-                'group relative',
-                item.position.span > 1 && 'bg-25-white/30',
-              )}
-              style={{
-                gridRow: `${item.position.start} / span ${item.position.span}`,
-              }}>
-              <EventItem
-                id={item.columnData.id}
-                trackId={header.id}
-                eventTime={{
-                  start: item.columnData.body.time.start,
-                  end: item.columnData.body.time.end,
-                }}
-                header={item.columnData.header}
-                footer={{
-                  leftButton: item.columnData.footer.infoButton,
-                  rightLink: item.columnData.footer.inscription,
-                }}
-                activityType={item.columnData.labels?.left}
-                duration={item.columnData.body.duration}
-                color={header.color}
-                location={header.name}
-              />
-            </div>
+            <EventGroup
+              key={`group-${header.id}-${item.position.start}`}
+              group={item}
+              header={{
+                id: header.id,
+                color: header.color,
+                name: header.name,
+              }}
+            />
           )
-        })}
-      </GridColumn>
-    )
-  })
+        }
+
+        return (
+          <li
+            key={item.columnData.id}
+            className={cn(
+              'group relative',
+              item.position.span > 1 && 'bg-25-white/30',
+            )}
+            style={{
+              gridRow: `${item.position.start} / span ${item.position.span}`,
+            }}>
+            <EventItem
+              id={item.columnData.id}
+              trackId={header.id}
+              eventTime={{
+                start: item.columnData.body.time.start,
+                end: item.columnData.body.time.end,
+              }}
+              header={item.columnData.header}
+              footer={{
+                leftButton: item.columnData.footer.infoButton,
+                rightLink: item.columnData.footer.inscription,
+              }}
+              activityType={item.columnData.labels?.left}
+              duration={item.columnData.body.duration}
+              color={header.color}
+              location={header.name}
+            />
+          </li>
+        )
+      })}
+    </GridColumn>
+  ))
 }
