b7b80cbea3b332b60fe006b7d77ae92d635ed7cc
Strocs <strocsdev@gmail.com>
2025-10-08 15:15:30 -0300

    chore(arc2025): remove old schedule section and data

--- src/features/arc2025/sections/schedule/data/day1/index.ts
+++
@@ -1,4 +0,0 @@
-import type { Arc2025Event } from '../../types'
-import { events } from './events'
-
-export const day1Events: Arc2025Event[] = events
--- src/features/arc2025/sections/schedule/data/day2/index.ts
+++
@@ -1,4 +0,0 @@
-import type { Arc2025Event } from '../../types'
-import { events } from './events'
-
-export const day2Events: Arc2025Event[] = events
