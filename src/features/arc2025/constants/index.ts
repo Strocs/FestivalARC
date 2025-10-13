3b3bad6ae44d626ed2df26e98e8562a88232bd86
Strocs <strocsdev@gmail.com>
2025-10-12 22:39:56 -0300

    refactor(arc2025): create constants values to avoid magic strings

---
+++ src/features/arc2025/constants/index.ts
@@ -0,0 +1,8 @@
+export const FESTIVAL_DAYS = ['Sábado 18', 'Domingo 19']
+export const FESTIVAL_PLACE = 'Parque Pedro de Valdivia'
+export const FESTIVAL_CITY = 'La Serena'
+export const FESTIVAL_DAYS_SHORT = '18 y 19 de Octubre'
+export const FESTIVAL_TICKETS_URL = [
+  'https://www.portaldisc.com/evento/festivalarc2025-18oct',
+  'https://www.portaldisc.com/evento/festivalarc2025-19oct',
+]
